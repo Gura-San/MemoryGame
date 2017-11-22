2e255794e7b6c87cfb6663028014ace061c95086
Sandro Guraspasvili <gura.san@gmail.com>
2017-11-22 13:48:36 -0500

    work from school unit 10

--- js/main.js
+++ js/main.js
@@ -1,15 +1,39 @@
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+    {
+      rank: 'queen',
+      suit: 'hearts',
+      cardImage: 'images/queen-of-hearts.png'
+    },
+    {
+      rank: 'queen',
+      suit: 'diamonds',
+      cardImage: 'images/queen-of-diamonds.png'
+    },
+    {
+      rank: 'king',
+      suit: 'hearts',
+      cardImage: 'images/king-of-hearts.png'
+    },
+    {
+      rank: 'king',
+      suit: 'diamonds',
+      cardImage: 'images/king-of-diamonds.png'
+    }
+  ]
+
 var cardsInPlay = [];
 
-flipCard(2);
+flipCard(0);
 flipCard(2);
 checkForMatch();
 
 function flipCard(cardId) {
 
-    console.log(cards[cardId] + " was flipped");
-    cardsInPlay.push(cardId);
+    console.log(cards[cardId].rank + ' was flipped')
+    cardsInPlay.push(cardId).rank
+    console.log(cards[cardId].cardImage)
+    console.log(cards[cardId].suit)
 
 }
 
